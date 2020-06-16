331abd82b0201c400982f5177046f628a5daeffa
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-06-16 21:02:30 +0530

    updating index.js

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.set('views','./views');
 
 app.listen(port,function(err,data){
     if(err){
-        console.log("Error while Listening ", err);
+        console.log("Error in running the server on Listening ", err);
         return ;
     }
-    console.log("Succcussfully connected");
+    console.log(`Succcussfully connected to ${port}`);
 });
