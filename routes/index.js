74cad62c483062eef7efb00797aac83b79d5c90b
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-06-16 21:47:27 +0530

    creating user controller and routes

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const homeController = require('../controllers/home_controller');
 console.log("routes running");
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
 
 
 module.exports =  router;
