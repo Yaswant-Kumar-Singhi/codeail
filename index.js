6561882039bc7f9f7be6de71a2fb559797380896
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-06-16 21:28:29 +0530

    created my first controller for home

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-// Creating Express
+// requiring libraries
 const express = require('express');
 const db = require('./config/mongoose');
 
 //setting up the port number where my application will run
 const port = 8000;
 
+//making express function
 app = express();
 
 // use express router
 app.use('/',require('./routes'));
 
 
-
+//server on
 app.listen(port,function(err,data){
     if(err){
         console.log("Error in running the server on Listening ", err);
