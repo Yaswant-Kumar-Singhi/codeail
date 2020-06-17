22d7d3eebab52e912fab77dc16a83d47e517b27e
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-06-17 20:27:21 +0530

    setting up manual auth

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,3 +3,27 @@ module.exports.profile = function(req,res){
         title : 'user controller working. Ejs loaded successfully '
     })
 }
+
+//render the sign up page
+module.exports.signup = function(req,res){
+    return res.render('user_sign_up',{
+        title : "Codeail | Sign Up"
+    })
+}
+
+
+//render the sign in page
+module.exports.signin = function(req,res){
+    return res.render('user_sign_in',{
+        title : "Codeail | Sign In"
+    })
+}
+
+//get the sign up data
+module.exports.create = function(req,res){
+    //TODO LATER
+}
+
+module.exports.createSession = function(req,res){
+    //TODO LATER
+}
