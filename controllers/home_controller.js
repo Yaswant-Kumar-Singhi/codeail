d58e0ffb4009350c45e79d53b4e9bb48ef6d273c
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-06-21 00:06:15 +0530

    updating username in post wall

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,15 +3,14 @@ const Post = require('../model/post');
 module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
-
+/*
      Post.find({}, function(err, post){
          return res.render('home', {
              title: "Codeial | Home",
              post:  post
          });
      });
-
-    /*
+ */
     // populate the user of each post
     Post.find({}).populate("user").exec(function(err, post){
         return res.render('home', {
@@ -19,7 +18,7 @@ module.exports.home = function(req, res){
             post:  post
         });
     })
-*/
+
 }
 
 // module.exports.actionName = function(req, res){}
